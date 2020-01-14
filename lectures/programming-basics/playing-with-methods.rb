0bb13977b7565c1f01e302645457637bc6de30ba
Matheus Penchel <matheus.penchel@gmail.com>
2020-01-14 10:12:00 -0300

    First commit

---
+++ lectures/programming-basics/playing-with-methods.rb
@@ -0,0 +1,43 @@
+require 'date'
+
+def greet(name)
+  return "Greetings, old friend #{name}"
+end
+
+puts greet("Matt")
+puts greet("Milene")
+
+driver_name = "Pedro"
+puts greet(driver_name)
+
+
+def full_name(first_name, last_name)
+  name = "#{first_name.capitalize} #{last_name.capitalize}"
+  name
+end
+
+puts full_name("MATHEUS", "PENCHEL")
+
+
+def bigger_number(x, y)
+  if x > y
+    return x
+  else
+    return y
+  end
+end
+
+puts bigger_number(14, 2)
+
+first_n = 20
+second_n = 30
+
+max_number = bigger_number(first_n, second_n)
+puts max_number
+
+def tomorrow
+  tomorrow_date = Date.today + 1
+  tomorrow_date.strftime('%m/%d/%Y')
+end
+
+puts tomorrow
---
+++ lectures/programming-basics/testing-around.rb
@@ -0,0 +1,19 @@
+'Mastodon' # String
+"Mastodon" # String
+
+14 # Integer
+14.5 # Float
+
+true  # Boolean (Actually, TrueClass)
+false # Boolean (Actually, FalseClass)
+
+[] # Array
+[1, 3, "Whatever"] # Array
+
+
+age = 17
+
+puts "Hey, you're really young, you're #{age} years old"
+
+age = age + 1
+puts "Happy birthday, you're now #{age} years old"
